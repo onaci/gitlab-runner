46642ff41c370102ea86292a8498baebd3bc2840
Dowideit, Sven (O&A, St. Lucia) <Sven.Dowideit@csiro.au>
2019-07-16 13:15:31 +1000

    add some debug output
    Signed-off-by: Dowideit, Sven (O&A, St. Lucia) <Sven.Dowideit@csiro.au>

--- register_and_run.sh
+++ register_and_run.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
+#!/bin/bash -ex
+
+echo "GITLABURL: ${GITLABURL}"
+echo "RUNNERTOKEN: ${RUNNERTOKEN}"
+echo "EXECUTOR: ${EXECUTOR:-docker}"
+echo "DOCKERIMAGE: ${DOCKERIMAGE:-alpine:latest}"
+echo "RUNNERNAME: ${RUNNERNAME:-gitlab runner}"
+echo "TAGLIST: ${TAGLIST:-docker,auto}"
 
 gitlab-runner register \
-   --non-interactive \ 
-   --url "${GITLABURL}" \
-   --registration-token "${RUNNERTOKEN}" \
-   --executor "${EXECUTOR:-docker}" \
-   --docker-image "${DOCKERIMAGE:-alpine:latest}" \
-   --description "${RUNNERNAME:-gitlab runner}" \
-   --tag-list "${TAGLIST:-docker,auto}" \
-   --run-untagged="true" \
-   --locked="false" \
-   --access-level="not_protected"
+    --non-interactive \
+    --url "${GITLABURL}" \
+    --registration-token "${RUNNERTOKEN}" \
+    --executor "${EXECUTOR:-docker}" \
+    --docker-image "${DOCKERIMAGE:-alpine:latest}" \
+    --description "${RUNNERNAME:-gitlab runner}" \
+    --tag-list "${TAGLIST:-docker,auto}" \
+    --run-untagged="true" \
+    --locked="false" \
+    --access-level="not_protected"
 
 exec /entrypoint "$@"
